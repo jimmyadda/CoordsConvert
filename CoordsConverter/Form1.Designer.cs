cf572d544b256945a885a296c994d68ab38f4832
jimmy adda <addajimmy@gmail.com>
2022-01-31 15:08:42 +0200

    first

--- CoordsConverter/Form1.Designer.cs
+++ CoordsConverter/Form1.Designer.cs
@@ -28,13 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.FilePath = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CoordsList1 = new System.Windows.Forms.ListBox();
+            this.CoordsList2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FilePath
+            // 
+            this.FilePath.Location = new System.Drawing.Point(24, 27);
+            this.FilePath.Multiline = true;
+            this.FilePath.Name = "FilePath";
+            this.FilePath.Size = new System.Drawing.Size(511, 26);
+            this.FilePath.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(541, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Load File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "List of Coords";
+            // 
+            // CoordsList1
+            // 
+            this.CoordsList1.FormattingEnabled = true;
+            this.CoordsList1.Location = new System.Drawing.Point(24, 105);
+            this.CoordsList1.Name = "CoordsList1";
+            this.CoordsList1.Size = new System.Drawing.Size(207, 316);
+            this.CoordsList1.TabIndex = 4;
+            this.CoordsList1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.CoordsList1_KeyDown);
+            // 
+            // CoordsList2
+            // 
+            this.CoordsList2.FormattingEnabled = true;
+            this.CoordsList2.Location = new System.Drawing.Point(409, 105);
+            this.CoordsList2.Name = "CoordsList2";
+            this.CoordsList2.Size = new System.Drawing.Size(207, 316);
+            this.CoordsList2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(406, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "New List of Coords";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(279, 227);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 26);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Convert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(637, 395);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(126, 26);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "SaveToTxt";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.CoordsList2);
+            this.Controls.Add(this.CoordsList1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.FilePath);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox FilePath;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox CoordsList1;
+        private System.Windows.Forms.ListBox CoordsList2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- CoordsConverter/Form1.cs
+++ CoordsConverter/Form1.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using Coordinates;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace CoordsConverter
 {
@@ -15,6 +18,90 @@ namespace CoordsConverter
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+                    var lines = File.ReadAllLines(filePath);
+                    for (var i = 0; i < lines.Length; i += 1)
+                    {
+                        var line = lines[i];
+                        CoordsList1.Items.Add(line);
+                    }
+                }
+            }
+
+            FilePath.Text = filePath;
+        }
+
+        private void CoordsList1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && (e.KeyCode == Keys.C))
+            {
+                Clipboard.SetText(this.CoordsList1.SelectedItem.ToString());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach(var item in CoordsList1.Items)
+            {
+                var x2 = item.ToString();
+                double x = double.Parse(item.ToString().Split(',')[0]);
+                double y = double.Parse(item.ToString().Split(',')[1]);
+
+                Converters.itm2wgs84((float)y, (float)x, out double lat, out double lon);
+
+                var Mylat = lat;
+                var Mylon = lon;
+                var new_Coords = Mylat.ToString() + ',' + Mylon.ToString();
+                CoordsList2.Items.Add(new_Coords);
+
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog oSaveFileDialog = new SaveFileDialog();
+            oSaveFileDialog.Filter = "All files (*.*) | *.*";
+            if (oSaveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = oSaveFileDialog.FileName;
+                string extesion = Path.GetExtension(fileName);
+                StreamWriter sw = new StreamWriter(oSaveFileDialog.OpenFile());
+                if(sw!=null) {
+                foreach (var it in CoordsList2.Items)
+                {
+                        sw.WriteLine(it);
+                }                    
+                    sw.Close();
+                }
+            }
+
+            MessageBox.Show(string.Format("File Saved in {0}", oSaveFileDialog.FileName));
         }
     }
 }
